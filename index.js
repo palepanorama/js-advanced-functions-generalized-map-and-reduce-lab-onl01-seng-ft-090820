f9e3dabab24598aac238ec871c9aff4961d96bd8
palepanorama <matthew.keith.thomas@gmail.com>
2021-01-03 21:29:04 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+const map = (arr, fns) => {
+    let newArr = []
+    for (const element of arr) {
+        newArr.push(fns(element))
+    }
+    return newArr;
+}
+
+const reduce = (arr, fns, begin) => {
+    let i = 1
+    let total
+    if (begin) {
+        i = 0
+        total = begin
+    } else {
+        total = arr[0]
+    }
+    for (i; i < arr.length; i++) {
+        total = fns(total, arr[i])
+    }
+    return total;
+}
